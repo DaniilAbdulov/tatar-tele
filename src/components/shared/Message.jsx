ea06b5018904df0bf888ee2b4968c3ee9985c7d2
Daniil Abdulov <abdulov.dany@yandex.com>
2024-04-27 17:20:31 +0500

    change VH

--- src/components/shared/Message.jsx
+++ src/components/shared/Message.jsx
@@ -1,52 +1,48 @@
-import React, { useEffect } from 'react';
-import { Button, message } from 'antd';
-import { firstLessonStore } from '../../stores/components/FirstLesson/FirstLessonStore';
-import { observer } from 'mobx-react-lite';
+import React, { useEffect } from "react";
+import { Button, message } from "antd";
+import { firstLessonStore } from "../../stores/components/FirstLesson/FirstLessonStore";
+import { observer } from "mobx-react-lite";
 
 export const Message = observer(() => {
-  const [messageApi, contextHolder] = message.useMessage();
-  const result = firstLessonStore.result;
+    const [messageApi, contextHolder] = message.useMessage();
+    const result = firstLessonStore.result;
 
-  useEffect(() => {
-    const handleSuccess = () => {
-      messageApi.open({
-        type: 'success',
-        content: 'Әйбат булды !',
-        className: 'custom-class',
-        style: {
-          marginTop: '20vh',
-        },
-      });
-    };
+    useEffect(() => {
+        const handleSuccess = () => {
+            messageApi.open({
+                type: "success",
+                content: "Әйбат булды !",
+                className: "custom-class",
+                style: {
+                    marginTop: "10vh",
+                },
+            });
+        };
 
-    const handleError = () => {
-      messageApi.open({
-        type: 'error',
-        content: 'Неверный ответ',
-        className: 'custom-class',
-        style: {
-          marginTop: '20vh',
-        },
-      });
-    };
+        const handleError = () => {
+            messageApi.open({
+                type: "error",
+                content: "Неверный ответ",
+                className: "custom-class",
+                style: {
+                    marginTop: "10vh",
+                },
+            });
+        };
 
-    if (result === 'success') {
-      handleSuccess();
-      setTimeout(() => {
-        firstLessonStore.setResult('');
-        firstLessonStore.resetTask();
-        firstLessonStore.getAnotherTask();
-      }, 2000);
-    } else if (result === 'error') {
-      handleError();
-      setTimeout(() => {
-        firstLessonStore.setResult('');
-      }, 2000);
-    }
-  }, [result, messageApi]);
-  return (
-    <>
-      {contextHolder}
-    </>
-  );
+        if (result === "success") {
+            handleSuccess();
+            setTimeout(() => {
+                firstLessonStore.setResult("");
+                firstLessonStore.resetTask();
+                firstLessonStore.getAnotherTask();
+            }, 2000);
+        } else if (result === "error") {
+            handleError();
+            setTimeout(() => {
+                firstLessonStore.setResult("");
+            }, 2000);
+        }
+    }, [result, messageApi]);
+    return <>{contextHolder}</>;
 });
