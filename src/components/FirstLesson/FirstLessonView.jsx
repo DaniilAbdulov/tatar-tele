6da0b874a08a4385f6d641742b40c910d81f5eb1
Daniil Abdulov <abdulov.dany@yandex.com>
2024-04-27 17:12:19 +0500

    styles and fix letters

--- src/components/FirstLesson/FirstLessonView.jsx
+++ src/components/FirstLesson/FirstLessonView.jsx
@@ -5,14 +5,18 @@ import { NavLink } from "react-router-dom";
 import { Message } from "../shared/Message";
 
 export const FirstLessonView = () => (
-    <Flex vertical={true} align='center' style={{maxWidth: '600px', margin: '10px'}}>
-      <Card
-        title='Просклоняейте глагол'
-      >
-        <Content />
-      </Card>
-      <Divider />
-      <Button><NavLink to='/'>К списку уроков</NavLink></Button>
-      <Message />
+    <Flex
+        vertical={true}
+        align="center"
+        style={{ maxWidth: "600px", margin: "10px" }}
+    >
+        <Card title="Просклоняйте глагол">
+            <Content />
+        </Card>
+        <Divider />
+        <Button>
+            <NavLink to="/">К списку уроков</NavLink>
+        </Button>
+        <Message />
     </Flex>
-  );
+);
--- src/views/Home.jsx
+++ src/views/Home.jsx
@@ -1,32 +1,31 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { observer } from "mobx-react-lite";
-import { Card} from "antd";
+import { Card, Flex } from "antd";
 import { homeStore } from "../stores/views/HomeStore.js";
 import { NavLink } from "react-router-dom";
+import "./Home.scss";
 
 export const Home = observer(() => {
     const lessons = homeStore.lessons;
-    const gridStyle = {
-        width: "25%",
-        textAlign: "center",
-        cursor: "pointer",
-    };
+
     return (
-        <Card title="Выберите урок">
-            {lessons &&
-                lessons?.length &&
-                lessons.map((lesson) => {
-                    return (
-                        <Card.Grid style={gridStyle} key={lesson.id}>
-                            <NavLink
-                                style={{ color: "black" }}
-                                to={lesson.path}
-                            >
-                                {lesson.title}
-                            </NavLink>
-                        </Card.Grid>
-                    );
-                })}
+        <Card title="Выберите урок" className="content">
+            <Flex wrap="wrap" gap="small">
+                {lessons &&
+                    lessons?.length &&
+                    lessons.map((lesson) => {
+                        return (
+                            <Card key={lesson.id} className="card">
+                                <NavLink
+                                    style={{ color: "black" }}
+                                    to={lesson.path}
+                                >
+                                    {lesson.title}
+                                </NavLink>
+                            </Card>
+                        );
+                    })}
+            </Flex>
         </Card>
     );
 });
